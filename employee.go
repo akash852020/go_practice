6442e07aec59a36c3a642560418a5c8eb037a989
akash852020 <bansalakash877@gmail.com>
2024-07-10 22:02:57 +0530

    Update employee.go

--- employee.go
+++ employee.go
@@ -9,10 +9,4 @@ type Employee struct {
 	Email     string  `json:"email"`
 }
 
-// EmpName   string  `json:"Name"`
-// EmpSalary float64 `json:"salary"`
-// Email     string  `json:"email"`
 
-// EmpName   string  `gorm:"type:varchar(20)"`
-// EmpSalary float64 `gorm:"type:float"`
-// Email     string  `gorm:"type:longtext"`
